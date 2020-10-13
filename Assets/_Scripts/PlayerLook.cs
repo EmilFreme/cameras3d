4dd0b414aafb97fe162a7dc139d121bd796935f3
EmilFreme <pedrohepf@insper.edu.br>
2020-10-13 13:50:17 -0300

    Reorganização de cenas

--- Assets/_Scripts/PlayerLook.cs
+++ Assets/_Scripts/PlayerLook.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StaticMovable : MonoBehaviour
+public class PlayerLook : MonoBehaviour
 {
     Transform player;
     // Start is called before the first frame update
